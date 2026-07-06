1bd352048efee25ff8264f30d45860929723822c
Natheniel Mankanda <nathanielmankanda8@gmail.com>
2026-07-06 14:06:45 +0200

    Fixed Circles screen collisions

--- src/componenets/DvdSlider.tsx
+++ src/componenets/DvdSlider.tsx
@@ -27,6 +27,32 @@ const DvdSlider: React.FC = () => {
       position.current.x += velocity.current.x;
       position.current.y += velocity.current.y;
 
+      //to get items width
+      const screenWidth = window.innerWidth;
+      const imageWidth = image.offsetWidth;
+
+      const screenHeight = window.innerHeight;
+      const imageHeight = image.offsetHeight;
+
+
+      //bounce off left and right ends
+      if (position.current.x <= 0) {
+        velocity.current.x *= -1;
+      }
+
+      if (position.current.x >= screenWidth - imageWidth){
+        velocity.current.x *= -1
+      }
+
+      //Bounce off bottom and top
+      if (position.current.y <= 0){
+        velocity.current.y *= -1;
+      }
+
+      if (position.current.y >= screenHeight - imageHeight){
+        velocity.current.y *= -1
+      }
+
       image.style.transform = `
         translate(${position.current.x}px, ${position.current.y}px)
       `;
